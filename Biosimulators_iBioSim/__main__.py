4e1e30d7401b7fb3809aa3f4c928d060aa814661
Jonathan Karr <jonrkarr@gmail.com>
2020-10-29 01:51:00 -0400

    entering Docker image URL into tests; formatting author email address

--- Biosimulators_iBioSim/__main__.py
+++ Biosimulators_iBioSim/__main__.py
@@ -1,6 +1,6 @@
 """ BioSimulators-compliant command-line interface to the `iBioSim <https://github.com/MyersResearchGroup/iBioSim>`_ simulation program.
 
-:Author: Myers Research Group, chris.myers@colorado.edu
+:Author: Myers Research Group <chris.myers@colorado.edu>
 :Date: 2020-06-12
 :Copyright: 2020
 :License: Apache-2.0
