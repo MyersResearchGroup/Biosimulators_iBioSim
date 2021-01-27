cbb893d0b0e4f3fbce7af88a747d049586d8e001
Payton Thomas <paytonco@gmail.com>
2021-01-26 23:03:30 -0700

    Analysis flags and libraries added

--- Biosimulators_iBioSim/core.py
+++ Biosimulators_iBioSim/core.py
@@ -12,8 +12,7 @@ import shutil
 
 __all__ = ['exec_combine_archive']
 
-
-def exec_combine_archive(archive_file, out_dir):
+def exec_combine_archive(archive_file, out_dir, directory, properties, inittime, limtime, outtime, printinterval, minstep, maxstep, abserr, relerr, seed, runs, simulation):
     """ Execute the SED tasks defined in a COMBINE archive and save the outputs
 
     Args:
@@ -21,11 +20,9 @@ def exec_combine_archive(archive_file, out_dir):
         out_dir (:obj:`str`): directory to store the outputs of the tasks
     """
     
-    
-    os.environ["BIOSIM"] = os.popen('pwd').read() + r"/.."
-    os.environ["PATH"] = os.environ["BIOSIM"]+r"/bin:"+os.environ["BIOSIM"]+r"/lib:"+os.environ["PATH"]    
+    os.environ["BIOSIM"] = "/iBioSim"
+    os.environ["PATH"] = os.environ["BIOSIM"]+r"/bin:"+os.environ["BIOSIM"]+r"/lib:"+os.environ["PATH"]
     os.environ["LD_LIBRARY_PATH"] = os.environ["BIOSIM"] + r"/lib:"
-    
     
     if not os.path.isfile(archive_file):
         raise FileNotFoundError("File does not exist: {}".format(archive_file))
@@ -33,18 +30,34 @@ def exec_combine_archive(archive_file, out_dir):
     '''if not zipfile.is_zipfile(archive_file):
         raise IOError("File is not an OMEX Combine Archive in zip format: {}".format(archive_file))'''
         
-        
-    cmd = r"java -jar /iBioSim/analysis/target/iBioSim-analysis-3.1.0-SNAPSHOT-jar-with-dependencies.jar -sim hode "
-    os.system(cmd+archive_file)
+    cmd = r"java -jar /iBioSim/analysis/target/iBioSim-analysis-3.1.0-SNAPSHOT-jar-with-dependencies.jar " #hode sim is java based
+    if not directory is None:
+        cmd += "-d " + directory
+    if not properties is None:
+        cmd += "-p " + properties
+    if not inittime is None:
+        cmd += "-ti " + inittime
+    if not limtime is None:
+        cmd += "-tl " + limtime
+    if not outtime is None:
+        cmd += "-ot " + outtime
+    if not printinterval is None:
+        cmd += "-pi " + printinterval
+    if not minstep is None:
+        cmd += "-m0 " + minstep
+    if not maxstep is None:
+        cmd += "-m1 " + maxstep
+    if not abserr is None:
+        cmd += "-aErr " + abserr
+    if not relerr is None:
+        cmd += "-sErr " + relerr
+    if not seed is None:
+        cmd += "-sd " + seed
+    if not runs is None:
+        cmd += "-r " + runs
+    if not simulation is None:
+        cmd += "-sim "  + simulation
+           
+    os.system(cmd+" "+archive_file)
     shutil.move(archive_file.rsplit('.',1)[0],out_dir)
     
-    
-    
-    
-    
-    
-    #completed all of these
-#os.environ[‘BIOSIM’] = os.environ[‘PWD’] + ‘/..’
-#os.environ[‘PATH’] = os.environ[‘BIOSIM’]+’/bin:’+os.environ[‘BIOSIM’]+’/lib:’+os.environ[‘PATH’]  
-#add ld_library thing should look like:
-#export LD_LIBRARY_PATH=$BIOSIM/lib:$LD_LIBRARY_PATH
--- Dependencies/newbuild.sh
+++ Dependencies/newbuild.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
 BUILD_DIR=$PWD
-ROOT=$BUILD_DIR/..
+ROOT=/iBioSim
 
 tar -xf libSBML-5.15.2-core-plus-packages-src.tar.gz
-
 tar -xf gsl-latest.tar.gz
-
 tar -xf GeneNet.tar.gz
-
 tar -xf reb2sac.tar.gz
 
 # Install gsl
@@ -35,12 +32,3 @@ cd ${BUILD_DIR}/reb2sac
 CFLAGS="-I${ROOT}/include -O0 -g3 -DNAME_FOR_ID" LDFLAGS="-L${ROOT}/lib -lm" ./configure --prefix=${ROOT}/ && \
     env WANT_AUTOCONF_2_5="1" WANT_AUTOMAKE_1_6="1" make -k -j1 install
 cp ${ROOT}/bin/reb2sac ${ROOT}/bin/reb2sac.linux64
-
-
-#export BIOSIM=$PWD/..
-	
-	
-#export PATH=$BIOSIM/bin:$BIOSIM/lib:$PATH
-
-
-#export LD_LIBRARY_PATH=$BIOSIM/lib:$LD_LIBRARY_PATH
